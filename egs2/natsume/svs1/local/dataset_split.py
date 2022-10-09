5368a646891656b209ae5e4c1850e973792933a8
yifeng <jerryuhoo@gmail.com>
2022-10-09 02:19:55 -0400

    data preprocess

--- egs2/natsume/svs1/local/dataset_split.py
+++ egs2/natsume/svs1/local/dataset_split.py
@@ -22,32 +22,11 @@ def dev_check(song):
 def test_check(song):
     return song in TEST_LIST
 
-def get_parser():
-    parser = argparse.ArgumentParser(
-        description="Prepare segments from HTS-style alignment files",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-    parser.add_argument("db_root", type=str, help="source dataset midi folder path")
-    parser.add_argument("save_dir", type=str, help="path of dataset split for save")
-    parser.add_argument("dev", type=float, help="absolute size of dev dataset")
-    parser.add_argument("eval1", type=float, help="absolute size of dev dataset")
-    return parser
 
-
-def find_files_by_extensions(root, exts=[]):
-    def _has_ext(name):
-        if not exts:
-            return True
-        name = name.lower()
-        for ext in exts:
-            if name.endswith(ext):
-                return True
-        return False
-
-    for path, _, files in os.walk(root):
-        for name in files:
-            if _has_ext(name):
-                yield os.path.join(path, name)
+def pack_zero(string, size=20):
+    if len(string) < size:
+        string = "0" * (size - len(string)) + string
+    return string
 
 
 def makedir(data_url):
@@ -57,62 +36,78 @@ def makedir(data_url):
     os.makedirs(data_url)
 
 
-if __name__ == "__main__":
-    args = get_parser().parse_args(sys.argv[1:])
-    if args.db_root[-1] != "/":
-        args.db_root = args.db_root + "/"
-
-    if args.save_dir[-1] != "/":
-        args.save_dir = args.save_dir + "/"
-
-    source_root_url = args.db_root
-
-    data_root_url = args.save_dir
-    data_train_url = data_root_url + "tr_no_dev_raw"
-    data_vaild_url = data_root_url + "dev_raw"
-    data_test_url = data_root_url + "eval_raw"
-    makedir(data_root_url)
-    makedir(data_train_url)
-    makedir(data_vaild_url)
-    makedir(data_test_url)
-
-    dataset = os.listdir(source_root_url)
-    dev = int(len(dataset) * args.dev)
-    eval = int(len(dataset) * args.eval1)
-    train = len(dataset) - dev - eval
-    print(f"trainset={train}, vaild_set={dev}, eval1_set={eval}.")
-    if train <= 0:
-        print("Error, train set is empty.")
-        exit()
-    random.shuffle(dataset)
-    # copyfile(source_file, destination_file)
-    train_set = [data for data in dataset if train_check(data[:-4])]
-    validation_set = [data for data in dataset if dev_check(data[:-4])]
-    test_set = [data for data in dataset if test_check(data[:-4])]
+def process_pho_info(phone):
+    info = open(phone, "r", encoding="utf-8")
+    label_info = []
+    pho_info = []
+    for line in info.readlines():
+        line = line.strip().split()
+        label_info.append(
+            "{} {} {}".format(float(line[0]), float(line[1]), line[2].strip())
+        )
+        pho_info.append(line[2].strip())
+    return " ".join(label_info), " ".join(pho_info)
 
 
-def transition(dataset, des_url):
-    # src_len = len(source_root_url)
-    path = [des_url + item for item in ["/wav/", "/mono_label/", "/midi/"]]
-    for p in path:
-        makedir(p)
-    for item in dataset:
-        wav_path = source_root_url + item
-        lab_path = wav_path.replace("wav/", "mono_label/").replace(".wav", ".lab")
-        midi_path = wav_path.replace("wav/", "midi/").replace(".wav", ".mid")
+def process_subset(src_data, subset, check_func, fs, wav_dump):
+    subfolder = os.listdir(src_data)
+    makedir(subset)
+    wavscp = open(os.path.join(subset, "wav.scp"), "w", encoding="utf-8")
+    utt2spk = open(os.path.join(subset, "utt2spk"), "w", encoding="utf-8")
+    label_scp = open(os.path.join(subset, "label"), "w", encoding="utf-8")
+    musicxmlscp = open(os.path.join(subset, "musicxml.scp"), "w", encoding="utf-8")
 
-        des_wav = path[0] + item
-        des_lab = path[1] + item.replace(".wav", ".lab")
-        des_midi = path[2] + item.replace(".wav", ".mid")
-
-        if wav_path[-3:] != "wav":
-            print("Error Path: ", wav_path)
+    for folder in subfolder:
+        if not os.path.isdir(os.path.join(src_data, folder)):
             continue
-        copyfile(wav_path, des_wav)
-        copyfile(lab_path, des_lab)
-        copyfile(midi_path, des_midi)
+        if not folder == "wav":
+            continue
+        wavs = os.listdir(os.path.join(src_data, folder))
+        for wav in wavs:
+            wavname = os.path.splitext(wav)[0]
+            if not check_func(wavname):
+                continue
+            utt_id = "{}_{}".format(UTT_PREFIX, pack_zero(wavname))
+            print("utt_id", utt_id)
+            cmd = "sox {}.wav -c 1 -t wavpcm -b 16 -r {} {}_bits16.wav".format(
+                os.path.join(src_data, "wav", wavname),
+                fs,
+                os.path.join(wav_dump, wavname),
+            )
+            os.system(cmd)
+
+            wavscp.write(
+                "{} {}\n".format(
+                    utt_id, os.path.join(wav_dump, "{}_bits16.wav".format(wavname))
+                )
+            )
+            utt2spk.write("{} {}\n".format(utt_id, UTT_PREFIX))
+            label_info, pho_info = process_pho_info(
+                os.path.join(src_data, "mono_label", "{}.lab".format(wavname))
+            )
+            label_scp.write("{} {}\n".format(utt_id, label_info))
+            musicxmlscp.write(
+                "{} {}\n".format(
+                    utt_id, os.path.join(src_data, "xml", "{}.xml".format(wavname))
+                )
+            )
 
 
-transition(train_set, data_train_url)
-transition(validation_set, data_vaild_url)
-transition(test_set, data_test_url)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Prepare Data for Oniku Database")
+    parser.add_argument("src_data", type=str, help="source data directory")
+    parser.add_argument("train", type=str, help="train set")
+    parser.add_argument("dev", type=str, help="development set")
+    parser.add_argument("test", type=str, help="test set")
+    parser.add_argument("--fs", type=int, help="frame rate (Hz)")
+    parser.add_argument(
+        "--wav_dump", type=str, default="wav_dump", help="wav dump directory"
+    )
+    args = parser.parse_args()
+
+    if not os.path.exists(args.wav_dump):
+        os.makedirs(args.wav_dump)
+
+    process_subset(args.src_data, args.train, train_check, args.fs, args.wav_dump)
+    process_subset(args.src_data, args.dev, dev_check, args.fs, args.wav_dump)
+    process_subset(args.src_data, args.test, test_check, args.fs, args.wav_dump)
