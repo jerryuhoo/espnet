ee7fb6c9b6fbb7af58cbbe0507ca25ed22a7ddf3
jerryuhoo <jerryuhoo@gmail.com>
2023-02-17 01:11:24 -0500

    fix config

--- egs2/TEMPLATE/svs1/svs.sh
+++ egs2/TEMPLATE/svs1/svs.sh
@@ -35,7 +35,7 @@ ngpu=1               # The number of gpus ("0" uses cpu, otherwise use gpu).
 gpu_id=0             # GPU_id, only works when ngpu=1
 num_nodes=1          # The number of nodes.
 nj=32                # The number of parallel jobs.
-inference_nj=32      # The number of parallel jobs in decoding.
+inference_nj=16      # The number of parallel jobs in decoding.
 gpu_inference=false  # Whether to perform gpu decoding.
 dumpdir=dump         # Directory to dump features.
 expdir=exp           # Directory to save experiments.
